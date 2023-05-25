8cd8a79ce2a33f784674f571a270374092fb5152
chisom promise <chimisepro@gmail.com>
2023-05-25 19:17:16 +0100

    feat: Update package.json and server files

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const rateLimiter = require('express-rate-limit');
+const config = require('config');
+const connectDb = require('./shared/connectDb');
+const {PORT} = require('./shared/config');
+const urlController = require('./controllers/url')
+const errorController = require('./controllers/errorHandler');
+
+const rateConfig = config.get('rateLimiting');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(rateLimiter({
+    standardHeaders: true,
+    message: 'You have reached the limit',
+    ...(rateConfig || {})
+}))
+
+urlController(app);
+
+app.use(errorController);
+
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on Port ${PORT}`);
+    })
+})
+
+
+
+
+
